aa31f7ceaf81816d2bffdd3896b982692b6948a4
Rui Guedes <ruiguedes12@hotmail.com>
2019-04-11 19:51:53 +0100

    Organized repo

--- scripts/Peer/InitPeers.sh
+++ scripts/Peer/InitPeers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PROTOCOL_VERSION="2.0"
+PROTOCOL_VERSION="1.0"
 
 gnome-terminal -- ./RMI.sh
 
-for i in {1..3}
+for i in {1..5}
 do
    gnome-terminal -- ./Peer.sh $PROTOCOL_VERSION $i "rmi-access-point$i"
 done
--- scripts/TestApp/Backup.sh
+++ scripts/TestApp/Backup.sh
@@ -2,6 +2,6 @@ CLASS_PATH=../../out/production/SDIS1819_T1
 MAIN="TestApp"
 RMI_ACCESS_POINT="rmi-access-point1"
 FILE=../../files/img.jpg
-REPLICATION_DEGREE=1
+REPLICATION_DEGREE=3
 
 java --class-path $CLASS_PATH $MAIN $RMI_ACCESS_POINT "BACKUP" $FILE $REPLICATION_DEGREE
